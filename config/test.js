7cd23cb8297bd4453d2ba4cdd04bbf30e0907c7c
Jessica Xu <jessica@lob.com>
2017-04-17 14:58:28 -0700

    chore(lint): adds linter and travis ci

--- config/test.js
+++ config/test.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   DB_HOST: 'localhost',
-	DB_NAME: 'sfmovies_test',
-	DB_PASSWORD: '',
-	DB_PORT: 5432,
-	DB_USER: 'sfmovies_user'
+  DB_NAME: 'sfmovies_test',
+  DB_PASSWORD: '',
+  DB_PORT: 5432,
+  DB_USER: 'sfmovies_user'
 };
