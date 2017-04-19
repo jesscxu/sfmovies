80a734500d150c0e10fd2f6aef39e31a683ca6de
Jessica Xu <jessica@lob.com>
2017-04-19 15:02:25 -0700

    feat(locations) add a locations table and get/create endpoints

---
+++ test/models/location.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Location = require('../../lib/models/location');
+
+describe('location model', () => {
+
+  describe('serialize', () => {
+
+    it('includes all of the necessary fields', () => {
+      const loc = Location.forge().serialize();
+
+      expect(loc).to.have.all.keys([
+        'id',
+        'name',
+        'object'
+      ]);
+
+    });
+
+  });
+
+});
+
